db8b9480a79ed60c71fe2013a4a78adbdd89ae18
SrTangente <josejuangalan@gmail.com>
2021-03-03 15:40:15 +0100

    Computing of basic descriptors

--- descriptors.py
+++ descriptors.py
@@ -1,9 +1,23 @@
-import networkx
-from networkx import *
+import networkx as nx
+import numpy as np
+from networkx.algorithms import approximation
+from networkx.algorithms import *
 
-toy_nets = []
-star = networkx.read_pajek('./toy/star.net')
-print(star.number_of_nodes())
-print(star.number_of_edges())
-node_list = list(star.nodes())
-degree
+net = nx.Graph(nx.read_pajek('./toy/star.net'))
+print(net.number_of_nodes())
+print(net.number_of_edges())
+node_list = list(net.nodes())
+print(net.degree(node_list))
+degrees = [x[1] for x in net.degree(node_list)]
+average_dg = np.average(degrees)
+max_dg = np.max(degrees)
+min_dg = np.min(degrees)
+
+print('Maximum degree: ', max_dg, ' Minimum degree: ', min_dg, ' Average degree: ', average_dg)
+
+print(approximation.average_clustering(net))
+print(clustering(net))
+
+print(degree_assortativity_coefficient(net))
+print(average_shortest_path_length(net))
+print(diameter(net))
