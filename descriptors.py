a567c84fc830cc7fa76a1a73d7d5b59ccaf3d8a4
Guillermocidm <39138459+Guillermocidm@users.noreply.github.com>
2021-03-14 10:59:00 +0100

    d

--- descriptors.py
+++ descriptors.py
@@ -4,29 +4,47 @@ import networkx as nx
 import numpy as np
 from networkx.algorithms import approximation
 from networkx.algorithms import *
-import csv
 
-for dir in ['/toy/', '/model/', '/real/']:
-    for f in listdir(dir):
-        net = nx.Graph(nx.read_pajek(dir+f))
-        n_nodes = net.number_of_nodes()
-        n_edges = (net.number_of_edges())
+import os
+import pandas as pd
+
+files = ['model','real','toy']
+df = pd.DataFrame(columns=['Graph', 'n_nodes', 'n_edges', 'min_degree', 'max_degree', 'av_degree', 'clustering_cof', 'assortativity',
+             'av_path_length', 'diameter'])
+
+for f in files:
+    graphs = os.listdir(f)
+    for g in graphs:
+        print(g)
+        path = './'+f+'/'+g
+
+        net = nx.Graph(nx.read_pajek(path))
+
+        d = dict()
+        d['Graph'] = g
+        d['n_nodes'] = net.number_of_nodes()
+        d['n_edges'] = net.number_of_edges()
+
+
         node_list = list(net.nodes())
 
-        degree_list = net.degree(node_list)
         degrees = [x[1] for x in net.degree(node_list)]
-
         average_dg = np.average(degrees)
         max_dg = np.max(degrees)
         min_dg = np.min(degrees)
 
+        d['min_degree'] = round(min_dg,4)
+        d['max_degree'] = round(max_dg,4)
+        d['av_degree'] =  round(average_dg,4)
+
         #print('Maximum degree: ', max_dg, ' Minimum degree: ', min_dg, ' Average degree: ', average_dg)
 
-        avg_clustering = (approximation.average_clustering(net))
-        clustering = clustering(net)
+        d['clustering_cof'] = round(approximation.average_clustering(net),4)
+        d['assortativity'] = round(degree_assortativity_coefficient(net),4)
+        d['av_path_length'] = round(average_shortest_path_length(net),4)
+        d['diameter'] = round(diameter(net),4)
 
-        assortativity = degree_assortativity_coefficient(net)
-        svg_shortest_path_len = average_shortest_path_length(net)
-        diameter= diameter(net)
 
-        # TODO, write into CSV
+        df = df.append(d, ignore_index=True)
+        df.to_csv('descriptors.csv', index=False)
+
